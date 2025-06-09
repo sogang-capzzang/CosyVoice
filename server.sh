4bd506ef9fb6b89e47a043c1d16a88a67e976ac5
l0k0y <144587049+l0k0y@users.noreply.github.com>
2025-06-09 17:33:24 +0900

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,12 +5,14 @@ function usage(
         echo "Options:"
         echo "  --model_dir   Specify the model directory (default: pretrained_models/CosyVoice2-0.5B-trt)"
         echo "  --port PORT   Specify the port number for the server (default: 8080)"
+        echo "  --use_vllm    Enable vLLM (default: disabled)"
     }
 )
 
 usage
 MODEL_DIR="pretrained_models/CosyVoice2-0.5B-trt"
 PORT=8080
+USE_VLLM=""
 while [[ $# -gt 0 ]]; do
     case $1 in
         --model_dir)
@@ -21,10 +23,14 @@ while [[ $# -gt 0 ]]; do
             PORT="$2"
             shift 2
             ;;
+        --use_vllm)
+            USE_VLLM="--use_vllm"
+            shift
+            ;;    
         *)
             usage
             exit 1
             ;;
     esac
 done
-python ./runtime/python/fastapi/server.py --model_dir $MODEL_DIR --port $PORT
+python ./runtime/python/fastapi/server.py --model_dir $MODEL_DIR --port $PORT $USE_VLLM
